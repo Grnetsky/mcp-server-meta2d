c93238a1ad243a56716eaf4a7814e75486758a93
Grnetsky <1101767893@qq.com>
2026-01-09 17:38:01 +0800

    feat: upload

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -2,13 +2,44 @@ import {z} from "zod";
 
 // zod: https://zod.dev/api#string-formats
 export const basePenSchema = z.object({
-    x: z.number().describe("X coordinate of the top-left corner"),
-    y: z.number().describe("Y coordinate of the top-left corner"),
-    width: z.number().describe("width of the square"),
-    height: z.number().describe("height of the square"),
-    text: z.string().optional().describe("Text to display on the square center"),
-    color: z.string().optional().describe("Color of the square in hex format"),
-    background: z.string().optional().describe("Background color of the square"),
+    x: z
+        .number()
+        .describe(
+            "X coordinate of the top-left corner"
+        ),
+    y: z
+        .number()
+        .describe(
+            "Y coordinate of the top-left corner"
+        ),
+    width: z
+        .number()
+        .describe(
+            "width of the square"
+        ),
+    height: z
+        .number()
+        .describe(
+            "height of the square"
+        ),
+    text: z
+        .string()
+        .optional()
+        .describe(
+            "Text to display on the square center"
+        ),
+    color: z
+        .string()
+        .optional()
+        .describe(
+            "Color of the square in hex format"
+        ),
+    background: z
+        .string()
+        .optional()
+        .describe(
+            "Background color of the square ,support hex format or image url"
+        ),
     // 完善其他的schema属性
 })
 
