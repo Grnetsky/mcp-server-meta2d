e0099105edef64c8eed7eead34f5b0ffaedf78b1
Grnetsky <1101767893@qq.com>
2026-01-09 17:33:20 +0800

    feat: upload

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -1,11 +1,15 @@
 import {z} from "zod";
 
+// zod: https://zod.dev/api#string-formats
 export const basePenSchema = z.object({
     x: z.number().describe("X coordinate of the top-left corner"),
     y: z.number().describe("Y coordinate of the top-left corner"),
     width: z.number().describe("width of the square"),
     height: z.number().describe("height of the square"),
-    text: z.string().describe("Text to display on the square center")
+    text: z.string().optional().describe("Text to display on the square center"),
+    color: z.string().optional().describe("Color of the square in hex format"),
+    background: z.string().optional().describe("Background color of the square"),
+    // 完善其他的schema属性
 })
 
 export type BasePenSchema = z.infer<typeof basePenSchema>
